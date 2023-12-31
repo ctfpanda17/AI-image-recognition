1109b8e1ec7e0246a325a3df527a16632bb58d4b
ctfpanda17 <r61291@gmail.com>
2023-12-31 17:10:47 +0800

    input src

--- test-orc.py
+++ test-orc.py
@@ -2,4 +2,13 @@ from PIL import Image
 import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
-print(pytesseract.image_to_string(Image.open("AI-image-recognition/testjpg/image.png"), lang='eng'))
+
+userinput = input("src=")
+
+def transform(userinput):
+    transform = userinput.replace("\\","/")
+    return transform
+
+src = transform(userinput)
+print(src)
+print(pytesseract.image_to_string(Image.open(src), lang='eng'))
