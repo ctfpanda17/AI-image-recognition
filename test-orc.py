a277c870b6211d7395e3ca199bc2c5d74fc764dd
ctfpanda17 <r61291@gmail.com>
2024-01-01 16:38:46 +0800

    num orc

--- test-orc.py
+++ test-orc.py
@@ -1,6 +1,9 @@
 from PIL import Image
 import pytesseract
+import cv2
+import numpy as np
 
+knn = cv2.ml.KNearest_load('opencv/mnist_knn.xml') 
 pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
 
 userinput = input("src=")
@@ -11,4 +14,14 @@ def transform(userinput):
 
 src = transform(userinput)
 print(src)
-print(pytesseract.image_to_string(Image.open(src), lang='eng'))
+print(pytesseract.image_to_string(Image.open(src), lang='eng', 
+                                                config='--psm 6 --oem 3 -c '
+                                                'tessedit_char_whitelist'
+                                                '=0123456789'))
+print(pytesseract.image_to_string(Image.open(src)))
+
+#text = str(int(knn.predict(src)))
+#fontFace = cv2.FONT_HERSHEY_SIMPLEX     # 印出的文字字體
+#fontScale = 2                           # 印出的文字大小
+#color = (0,0,255)                       # 印出的文字顏色
+#print(cv2.putText(text, fontFace, fontScale, color)) # 印出文字
